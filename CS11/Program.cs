1bedc0cc3697cda4bbfafd6b71ac119be1041997
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-18 16:52:35 +0300

    Добавьте файлы проекта.

---
+++ CS11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CS11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multipleThree = 3;
+            int multipleFive = 5;
+            int number;
+            Random random = new Random();
+
+            while (true)
+            {
+                number = random.Next(1, 101);
+                number /= multipleThree | multipleFive;
+                Console.WriteLine(number);
+                Console.ReadKey();
+            }
+        }
+    }
+}
