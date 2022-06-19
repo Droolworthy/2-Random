8ad89be2848675f76358b9b6c1723913f09ab3b9
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-19 16:38:55 +0300

    Update Program.cs

--- CS11/Program.cs
+++ CS11/Program.cs
@@ -6,16 +6,26 @@ namespace CS11
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int number = random.Next(0, 20);
             int divisibleNumber1 = 3;
             int divisibleNumber2 = 5;
-            int number;
-            Random random = new Random();
+            int sum = 0;
 
-            for(number = 0; number < 1; number++)
+            Console.WriteLine("Случайное число: " + number);
+            Console.WriteLine("Нажмите клавишу и увидите сумму всех положительных чисел меньше " + number + ", " + 
+                "которые кратные " + divisibleNumber1 + " или " + divisibleNumber2);
+            Console.ReadLine();
+
+            for (int i = 0; i < number; i++)
             {
-                number /= divisibleNumber1 | divisibleNumber2;
-                Console.WriteLine(random.Next(0,101));
+                if (i % divisibleNumber1 == 0 || i % divisibleNumber2 == 0 )
+                {
+                     sum = sum + i;
+                }
             }
+            Console.WriteLine(sum);
+            Console.ReadLine();
         }
     }
 }
